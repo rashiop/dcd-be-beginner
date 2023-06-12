688ebfb74efece0b8e7f5bc01b7ee319bb26194e
Poppy Sari <poppy.sari@rga.com>
2023-06-12 22:24:00 +0700

    07: add response body

--- learn/07-node-native-server/server.js
+++ learn/07-node-native-server/server.js
@@ -9,24 +9,44 @@ const PORT = 5000
  * @param response: objek yang digunakan untuk menanggapi permintaan
  */
 const requestListener = (request, response) => {
-  const { method } = request
+  const { url, method } = request
 
   response.setHeader('Content-Type', 'text/html')
+  response.setHeader('X-Powered-By', 'NodeJS')
   response.statusCode = 200
 
-  if (method === 'GET') {
-    return response.end('<h1>HTTP server GET</h1>')
-  }
-  if (method === 'POST') {
-    // Test
-    // curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Poppy\"}"
-    let body = []
-    request.on('data', (chunk) => body.push(chunk))
-    request.on('end', () => {
-      body = Buffer.concat(body).toString()
-      const { name } = JSON.parse(body) || {}
-      return response.end(`<h1>Hi ${name}</h1>`)
-    })
+  switch (url) {
+    case '/': {
+      if (method === 'GET') return response.end('Ini adalah homepage')
+
+      response.statusCode = 401
+      return response.end(JSON.stringify({ message: `Halaman tidak dapat diakses dengan ${method} request` }))
+    }
+    case '/about': {
+      if (method === 'GET') {
+        return response.end('<h1>Halo! Ini adalah halaman about</h1>')
+      }
+      if (method === 'POST') {
+        // Test
+        // curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Poppy\"}"
+        let body = []
+        request.on('data', (chunk) => body.push(chunk))
+        request.on('end', () => {
+          body = Buffer.concat(body).toString()
+          const { name } = JSON.parse(body) || {}
+          return response.end(JSON.stringify({ message: `Hi ${name}` }))
+        })
+      } else {
+        response.statusCode = 401
+        return response.end(JSON.stringify({ message: `Halaman tidak dapat diakses dengan ${method} request` }))
+      }
+      break
+    }
+    default: {
+      response.statusCode = 404
+      response.statusMessage = 'User is not found'
+      return response.end(JSON.stringify({ message: `User not found` }))
+    }
   }
 }
 
